cdccbddca10a5070602ad9ca86c2487b5875dd50
funzone36 <funzone36@gmail.com>
2016-07-12 17:50:15 -0400

    I can see the index

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,13 @@
+class PledgesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def new
+  end
+end
