4fc99bbf1aa13a6e76b05c837d1bd55315e6fcab
Leon Chiu <leontkchiu@gmail.com>
2016-07-13 16:57:51 -0400

    understand sessions path

--- crowdfunder/config/routes.rb
+++ crowdfunder/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # get 'sessions/new'
   #
-  # get 'sessions/create'
-  # # post 'sessions/'
+  # # get 'sessions/create'
+  # post 'sessions' => 'sessions#create'
   #
   # get 'sessions/destroy'
 
