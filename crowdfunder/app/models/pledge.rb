2b6e568eb3005bb1fea5495b6dcd9d558d5d7d04
David Cisneros <d.cisneros849@gmail.com>
2016-07-13 19:36:03 -0400

    added dealine validation

--- crowdfunder/app/models/pledge.rb
+++ crowdfunder/app/models/pledge.rb
@@ -1,4 +1,13 @@
 class Pledge < ActiveRecord::Base
+validate :deadline_past
+
   belongs_to :project
   belongs_to :user
+
+private
+
+  def deadline_past
+    if self.project.deadline.past?
+      errors.add("Deadline is past")
+  end
 end
