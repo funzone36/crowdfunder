688eec6b4b1a58541ca5e1c33d8a6b6628f79957
spencer <spencer.dossett@gmail.com>
2016-07-14 20:25:40 -0400

    added fully_funded? method to project model

--- crowdfunder/app/models/project.rb
+++ crowdfunder/app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ActiveRecord::Base
   has_many :rewards
   has_many :pledgies, through: :pledges, source: :users
 
+  # adds up all the funds from pledges
   def pledged_funds
     #
     # self.pledges.all.reduce do |sum, pledge|
@@ -11,14 +12,20 @@ class Project < ActiveRecord::Base
     # end
 
     result = 0
-
     self.pledges.all.each do |pledge|
       result += pledge.amount
     end
-
     result
 
   end
 
+  # checks to see if a project is fully funded
+  def fully_funded?
+    if self.pledged_funds >= self.goal
+      true
+    else
+      false
+    end
+  end
 
 end
