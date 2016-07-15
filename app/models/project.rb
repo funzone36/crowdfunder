e29a37d3a5b4acaeeea6d871f9e85475809fe9c7
spencer <spencer.dossett@gmail.com>
2016-07-15 15:00:09 -0400

    cleaned up files

--- app/models/project.rb
+++
@@ -1,6 +0,0 @@
-class Project < ActiveRecord::Base
-  has_many :pledges
-  belongs_to :user
-  has_many :rewards
-  has_many :pledgies, through: :pledges, source: :users
-end
--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :pledges
-  has_many :projects
-  has_many :pledged_projects, through: :pledges, source: :projects
-
-
-end
